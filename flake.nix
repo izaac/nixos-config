f43c7fcc2632a550a19bc3de462f127a2e7f87a1
izaac <jorge.izaac@gmail.com>
2026-01-24 13:04:53 -0700

    initial config

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Izaac NVIDIA NixOS Config";
+
+  inputs = {
+    # 1. The main NixOS source (Unstable branch for 50-series support)
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # 2. Home Manager (User-level configuration)
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      # This line is critical: it forces HM to use the same packages as your system
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations.ninja= nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        # Import your existing system config
+        ./configuration.nix
+
+        # Plug in Home Manager as a module
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          
+          # This tells HM which user it's managing
+          home-manager.users.izaac = import ./home.nix;
+        }
+      ];
+    };
+  };
+}
