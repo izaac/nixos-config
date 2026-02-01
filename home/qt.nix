2e5ae8dd9882b3f77c5d7c4ef029a3e87b01a668
Izaac Zavaleta <izaac.zavaleta@suse.com>
2026-01-31 20:16:10 -0700

    revert theme changes

--- home/qt.nix
+++ home/qt.nix
@@ -4,7 +4,7 @@
   qt = {
     enable = true;
     platformTheme.name = "kde";
-    style.name = "breeze-dark";
+    style.name = "breeze";
   };
 
   gtk = {
@@ -31,69 +31,6 @@
       color-scheme = "prefer-dark";
     };
   };
-
-  # Plasma Configuration via home-manager (using plain config files for now)
-  # Disabling animations and effects for raw speed
-  home.file.".config/kwinrc".text = ''
-    [org.kde.kdecoration2]
-    ButtonsOnLeft=
-    ButtonsOnRight=IAX
-    CloseButtonOnLeft=false
-    ThemeName=Breeze
-
-    [Compositing]
-    AnimationSpeedFactor=0
-    LatencyPolicy=LowLatency
-    
-    [Effect-Slide]
-    Duration=0
-
-    [Effect-Fade]
-    Duration=0
-    
-    [Effect-Scale]
-    Duration=0
-  '';
-  
-  home.file.".config/kdeglobals".text = ''
-    [KDE]
-    AnimationDurationFactor=0
-    LookAndFeelPackage=org.kde.breezedark.desktop
-
-    [General]
-    ColorScheme=BreezeDark
-    Name=Breeze Dark
-    
-    [Icons]
-    Theme=breeze-dark
-
-    [WM]
-    activeBackground=49,54,59
-    activeForeground=239,240,241
-    inactiveBackground=42,46,50
-    inactiveForeground=189,195,199
-
-    [Colors:Window]
-    BackgroundNormal=49,54,59
-    ForegroundNormal=239,240,241
-    
-    [Colors:View]
-    BackgroundNormal=35,38,41
-    ForegroundNormal=239,240,241
-    
-    [Colors:Button]
-    BackgroundNormal=49,54,59
-    ForegroundNormal=239,240,241
-
-    [Colors:Selection]
-    BackgroundNormal=61,174,233
-    ForegroundNormal=239,240,241
-  '';
-  
-  home.file.".config/plasmarc".text = ''
-    [Theme]
-    name=breeze-dark
-  '';
 
   # Ensure GTK apps use dark theme in KDE/Wayland
   home.sessionVariables = {
