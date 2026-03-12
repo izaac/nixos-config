bf702ec15929070d9d3642693cb28fe15b5d5c67
Izaac Zavaleta <izaac.zavaleta@suse.com>
2026-03-11 20:59:13 -0700

    quickfix for sparrow

--- overlays/dwarfs-fix.nix
+++ overlays/dwarfs-fix.nix
@@ -1,5 +1,8 @@
 final: prev: {
-  dwarfs = prev.dwarfs.override {
+  dwarfs = (prev.dwarfs.override {
     boost = prev.boost188;
-  };
+  }).overrideAttrs (old: {
+    doCheck = false;
+    cmakeFlags = (old.cmakeFlags or []) ++ [ "-DWITH_TESTS=OFF" ];
+  });
 }
