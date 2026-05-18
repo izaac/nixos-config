17ff2f26033cbc36b7e46e6afe41f2aa05d6692f
Izaac Zavaleta <izaac.zavaleta@suse.com>
2026-05-17 18:50:39 -0700

    installing brave with icons fix and making it the default

--- home/desktop.nix
+++ home/desktop.nix
@@ -83,11 +83,11 @@ in {
     mimeApps = {
       enable = true;
       defaultApplications = {
-        "text/html" = ["chromium-browser.desktop"];
-        "x-scheme-handler/http" = ["chromium-browser.desktop"];
-        "x-scheme-handler/https" = ["chromium-browser.desktop"];
-        "x-scheme-handler/about" = ["chromium-browser.desktop"];
-        "x-scheme-handler/unknown" = ["chromium-browser.desktop"];
+        "text/html" = ["brave-origin.desktop"];
+        "x-scheme-handler/http" = ["brave-origin.desktop"];
+        "x-scheme-handler/https" = ["brave-origin.desktop"];
+        "x-scheme-handler/about" = ["brave-origin.desktop"];
+        "x-scheme-handler/unknown" = ["brave-origin.desktop"];
 
         # Text
         "text/plain" = ["com.system76.CosmicEdit.desktop"];
