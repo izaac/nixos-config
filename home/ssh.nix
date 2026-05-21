217d894b3869016fd42f038d242ad217714469e8
Izaac Zavaleta <izaac.zavaleta@suse.com>
2026-05-20 21:20:11 -0700

    flake bump and fix ssh config warnings

--- home/ssh.nix
+++ home/ssh.nix
@@ -4,18 +4,16 @@
 
     enableDefaultConfig = false;
 
-    matchBlocks = {
+    settings = {
       "*" = {
-        addKeysToAgent = "yes";
-        identityFile = "~/.ssh/id_ed25519";
-        serverAliveInterval = 60;
-        serverAliveCountMax = 2;
-        extraOptions = {
-          StrictHostKeyChecking = "accept-new";
-          # Performance & Security Optimization
-          Ciphers = "aes128-gcm@openssh.com,chacha20-poly1305@openssh.com,aes256-gcm@openssh.com";
-          MACs = "hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,umac-128-etm@openssh.com";
-        };
+        AddKeysToAgent = "yes";
+        IdentityFile = "~/.ssh/id_ed25519";
+        ServerAliveInterval = 60;
+        ServerAliveCountMax = 2;
+        StrictHostKeyChecking = "accept-new";
+        # Performance & security optimization
+        Ciphers = "aes128-gcm@openssh.com,chacha20-poly1305@openssh.com,aes256-gcm@openssh.com";
+        MACs = "hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,umac-128-etm@openssh.com";
       };
     };
   };
