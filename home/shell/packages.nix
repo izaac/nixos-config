580190fbb89dbce2a3429f45d33375c1620ec11b
Izaac Zavaleta <izaac.zavaleta@suse.com>
2026-08-12 10:42:48 -0700

    Skip broken oxker mac os tests

--- home/shell/packages.nix
+++ home/shell/packages.nix
@@ -51,7 +51,7 @@ in {
       kubectl
       kdash # was k9s (Go) → kdash (Rust)
       k3d
-      oxker # was lazydocker (Go) → oxker (Rust)
+      (oxker.overrideAttrs (old: { doCheck = false; })) # skip broken macOS snapshot tests
       skopeo
       rclone
       rsync
