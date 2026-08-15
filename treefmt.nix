7b0bcb41b9a6a2e7126fe6fc0c33a509fd758707
Izaac Zavaleta <izaac.zavaleta@suse.com>
2026-08-15 11:16:00 -0700

    Add prettier, yamlfmt, taplo for more file types
    Enable prettier with proseWrap=always, printWidth=100 for md, yaml, json, jsonc.
    Add yamlfmt for yaml/yml (excludes secrets).
    Add taplo for toml files.
    Add explicit includes/excludes for each formatter.

--- treefmt.nix
+++ treefmt.nix
@@ -5,16 +5,45 @@ _: {
   ];
   programs = {
     alejandra.enable = true;
-    prettier.enable = true;
+    prettier = {
+      enable = true;
+      settings = {
+        proseWrap = "always";
+        printWidth = 100;
+        singleQuote = true;
+        tabWidth = 2;
+      };
+    };
     statix.enable = true;
     deadnix.enable = true;
     shfmt = {
       enable = true;
       indent_size = 2;
     };
+    taplo.enable = true;
+    yamlfmt.enable = true;
   };
-  settings.formatter.shfmt = {
-    options = ["-ci" "-bn"];
-    includes = ["*.sh" "*.bash" "*.envrc" "*.envrc.*" ".githooks/pre-commit"];
+  settings.formatter = {
+    shfmt = {
+      options = ["-ci" "-bn"];
+      includes = ["*.sh" "*.bash" "*.envrc" "*.envrc.*" ".githooks/pre-commit"];
+    };
+    prettier = {
+      includes = [
+        "*.md"
+        "*.yaml"
+        "*.yml"
+        "*.json"
+        "*.jsonc"
+      ];
+      excludes = ["secrets/*.yaml" "secrets/*.yml"];
+    };
+    taplo = {
+      includes = ["*.toml"];
+    };
+    yamlfmt = {
+      includes = ["*.yaml" "*.yml"];
+      excludes = ["secrets/*.yaml" "secrets/*.yml"];
+    };
   };
 }
